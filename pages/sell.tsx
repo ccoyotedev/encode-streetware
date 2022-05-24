81ad330c41fa72639a8717fecb60b80f37fb2b2b
Caleb Brown <44173285+cgcbrown@users.noreply.github.com>
2022-05-24 09:57:59 +0100

    Correct provider added

--- pages/sell.tsx
+++ pages/sell.tsx
@@ -1,30 +1,22 @@
-import type { ReactElement } from 'react'
-import Link from 'next/link'
-import { GetStaticProps } from 'next'
-import {
-  } from '@chakra-ui/react'
-import Layout from '../components/layout'
-import NestedLayout from '../components/nested-layout'
-// import { useWeb3 } from '../contexts/useWeb3'
+import type { ReactElement } from "react";
+import {} from "@chakra-ui/react";
+import Layout from "../components/layout";
+import NestedLayout from "../components/nested-layout";
+import { useWeb3React } from "@web3-react/core";
+import { providers } from "ethers";
 
+const Sell = () => {
+  const { library } = useWeb3React<providers.Web3Provider>();
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  // const { state } = useWeb3()
-}
-
-const Sell = ( props: any) => {
-  return (
-    <></>
-  )
-}
-   
+  return <></>;
+};
 
 Sell.getLayout = function getLayout(page: ReactElement) {
-    return (
-      <Layout title="sell">
-        <NestedLayout>{page}</NestedLayout>
-      </Layout>
-    )
-}
-    
-export default Sell
+  return (
+    <Layout title="sell">
+      <NestedLayout>{page}</NestedLayout>
+    </Layout>
+  );
+};
+
+export default Sell;
